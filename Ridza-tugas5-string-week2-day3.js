382314854efb299b8a7e0f70293a7bab7ebcf354
Ridza Adhandra <andradhandra@gmail.com>
2019-10-24 16:40:18 +0700

    Tugas 5 Ridza Bermain dengan String

--- Ridza-tugas5-string-week2-day3.js
+++ Ridza-tugas5-string-week2-day3.js
@@ -21,7 +21,7 @@ var word = 'wow JavaScript is so cool';
 var exampleFirstWord = word[0] + word[1] + word[2];
 var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; // do your own!
 var thirdWord = word[15] + word[16]; // do your own!
-var fourthWord = word[19] + word[19]; // do your own!
+var fourthWord = word[16] + word[19]; // do your own!
 var fifthWord = word[21] + word[22] + word[23] + word[24]; // do your own!
 
 console.log('First Word: ' + exampleFirstWord);
@@ -54,7 +54,7 @@ console.log();
 console.log("Problem 4");
 
 var word4 = 'wow JavaScript is so cool';
-var exampleFirstWord4 = word.substring(0, 3);
+var exampleFirstWord4 = word4.substring(0, 3);
 var secondWord4 = word4.substring(4, 14); // do your own!
 var thirdWord4  = word4.substring(15, 17); // do your own!
 var fourthWord4 = word4.substring(18, 20); // do your own!
@@ -67,8 +67,8 @@ var thirdWord4length  = thirdWord4.length;
 var fourthWord4length = fourthWord4.length;
 var fifthWord4length  = fifthWord4.length;
 
-console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
-console.log('Second Word: ' + secondWord + ', with length: ' + secondWord4length);
-console.log('Third Word: ' + thirdWord   + ', with length: ' + thirdWord4length);
-console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWord4length);
-console.log('Fifth Word: ' + fifthWord   + ', with length: ' + fifthWord4length);
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWord4length);
+console.log('Third Word: ' + thirdWord4   + ', with length: ' + thirdWord4length);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWord4length);
+console.log('Fifth Word: ' + fifthWord4   + ', with length: ' + fifthWord4length);
